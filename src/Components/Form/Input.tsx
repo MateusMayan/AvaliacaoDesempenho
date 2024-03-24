2effa77aac7c56a369707441ee46f6e13cb37ace
MateusMayan <mateusmayan@icloud.com>
2024-03-24 15:57:08 -0300

    feat: added autocomplete Props in Input

--- src/Components/Form/Input.tsx
+++ src/Components/Form/Input.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { InputHTMLAttributes } from 'react';
 
-interface InputProps {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   type: string;
   name: string;
@@ -8,7 +8,8 @@ interface InputProps {
   onChange: React.ChangeEventHandler<HTMLInputElement>;
   error: string | null;
   onBlur: React.ChangeEventHandler<HTMLInputElement>;
-  placeholder?: string | undefined;
+  placeholder?: string;
+  autocomplete?: string;
 }
 
 const Input: React.FC<InputProps> = ({
@@ -20,6 +21,7 @@ const Input: React.FC<InputProps> = ({
   error,
   onBlur,
   placeholder,
+  autocomplete,
 }) => {
   return (
     <div>
@@ -31,12 +33,13 @@ const Input: React.FC<InputProps> = ({
       <input
         id={name}
         name={name}
-        className="block mb-5 w-full bg-gray-300 rounded-md py-1 px-2 duration-200 border focus:outline-none focus:border-blue-400 focus:shadow-sm focus:shadow-blue-100 focus:bg-gray-100"
+        className="block mb-5 w-full bg-gray-300 rounded-md py-1 px-2 duration-200 border focus:outline-none focus:border-blue-400 focus:shadow-sm focus:shadow-blue-100 focus:bg-gray-100 "
         type={type}
         value={value}
         onChange={onChange}
         onBlur={onBlur}
         placeholder={placeholder}
+        autoComplete={autocomplete}
       />
       {error && <p className="text-red-600 text-sm">{error}</p>}
     </div>
